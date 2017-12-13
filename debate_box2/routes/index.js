bac10f92287bca730bf17cebc1553952285e88e8
tcallred <taylor.allred21@gmail.com>
2017-12-13 21:50:33 +0000

    Now works with login and session

--- debate_box2/routes/index.js
+++ debate_box2/routes/index.js
@@ -1,14 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var expressSession = require('express-session');
 
-
+var users = require('../controllers/users_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.sendFile(__dirname + "/public/index.html");
+    console.log("/ Route");
+    //    console.log(req);
+    console.log(req.session);
+    if (req.session.user) {
+        console.log("/ Route if user");
+        console.log(req.session.username)
+        res.render('index', {
+            username: req.session.username
+        });
+    } else {
+        console.log("/ Route else user");
+        req.session.msg = 'Access denied!';
+        res.redirect('/login');
+    }
 });
 
+router.get('/login', function(req, res) {
+    console.log("/login Route");
+    if (req.session.user) {
+        res.redirect('/');
+    }
+    res.render('login', { msg: req.session.msg });
+});
 
+router.get('/logout', function(req, res) {
+    console.log("/logout Route");
+    req.session.destroy(function() {
+        res.redirect('/login');
+    });
+});
 
+router.get('/signup', function(req, res) {
+    console.log("/signup Route");
+    if (req.session.user) {
+        res.redirect('/');
+    }
+    res.render('signup', { msg: req.session.msg });
+});
+
+router.post('/signup', users.signup);
+router.post('/login', users.login);
 
 module.exports = router;
